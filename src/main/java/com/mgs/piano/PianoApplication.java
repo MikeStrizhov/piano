c66fc2e588411b87f0faeb37e4a51778ee9d64c7
M. Strizhov <m.strizhov@pflb.com>
2019-09-09 17:34:44 +0400

    Added custom Error Page in Thymeleaf

--- src/main/java/com/mgs/piano/PianoApplication.java
+++ src/main/java/com/mgs/piano/PianoApplication.java
@@ -3,16 +3,23 @@ package com.mgs.piano;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class PianoApplication {
+public class PianoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PianoApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PianoApplication.class);
+    }
 
 	@Bean
 	public RestTemplate restTemplate(){
--- src/main/java/com/mgs/piano/web/StackOverController.java
+++ src/main/java/com/mgs/piano/web/StackOverController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.constraints.Min;
@@ -39,4 +40,8 @@ public class StackOverController {
         return "result";
     }
 
+    @RequestMapping("/error1")
+    public void handleRequest() {
+        throw new RuntimeException("test exception");
+    }
 }
